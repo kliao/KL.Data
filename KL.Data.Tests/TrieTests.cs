3a9f49f676e70c440c7c1d95cfe8d569bd1e433c
kliao <kevintliao@gmail.com>
2015-11-17 21:40:58 -0500

    adding missing files

---
+++ KL.Data.Tests/TrieTests.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Linq;
+using NUnit.Framework;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using KL.Data.Trie;
+
+namespace KL.Data.Tests
+{
+    public class TrieTests : BaseTrieTest
+    {
+        [Test]
+        [ExpectedException(typeof(AggregateException))]
+        [Explicit]
+        public void ExhaustiveParallelAddFails()
+        {
+            while (true)
+            {
+                ITrie<char,int> trie = CreateTrie();
+                LongPhrases40
+                    .AsParallel()
+                    .ForAll(phrase => trie.Add(phrase.ToCharArray(), phrase.GetHashCode()));
+            }
+        }
+
+        protected override ITrie<char, int> CreateTrie()
+        {
+            return new Trie<char,int>();
+        }
+    }
+    public class KeyWordsTrieTest : BaseKeyWordsTrieTest
+    {
+        protected override ITrie<string, int> CreateTrie()
+        {
+            return new Trie<string, int>();
+        }
+    }
+
+    public class FoldingTrieTest : BaseKeyWordsTrieTest
+    {
+
+        protected override ITrie<string, int> CreateTrie()
+        {
+            return new FoldingTrie<string, int>();
+        }
+
+        [TestCase(new string[] { "hello", "world" }, 0)]
+        [TestCase(new string[] { "hello", "world", "!" }, 1)]
+        [TestCase(new string[] { "hello", "there", "world" }, 2)]
+        [TestCase(new string[] { "hi", "world" }, 3)]
+        [TestCase(new string[] { "hi", "world", "!" }, 7 )]
+        public void FoldAddTest(string[] query, int expected)
+        {
+            TestFunction<int, int, int>(expected, query, 0, (acc, data) => acc + data, x => x);
+        }
+
+        [TestCase(new string[] { "hello", "world" }, 0)]
+        [TestCase(new string[] { "hello", "world", "!" }, 0)]
+        [TestCase(new string[] { "hello", "there", "world" }, 2)]
+        [TestCase(new string[] { "hi", "world" }, 3)]
+        [TestCase(new string[] { "hi", "world", "!" }, 12)]
+        public void FoldMultiplicationTest(string[] query, int expected)
+        {
+            TestFunction<int, int, int>(expected, query, 1, (acc, data) => acc * Math.Max(data, 1), x => x);
+        }
+
+        private void TestFunction<TAccumulate, TData, TResult>(
+            TResult expected,
+            string[] query,
+            TAccumulate seed, 
+            Func<TAccumulate, TData, TAccumulate> folder, 
+            Func<TAccumulate, TResult> resultSelector)
+        {
+            Console.WriteLine(string.Join(",", query));
+            IFoldingTrie<string, TData> foldingTree = (FoldingTrie<string, TData>)Trie;
+            TResult actual = foldingTree.Fold<TAccumulate, TResult>(query, seed, folder, resultSelector);
+            NUnit.Framework.Assert.AreEqual(expected, actual);
+        }
+    }
+
+
+}
---
+++ KL.Data/Trie/FoldingTrie.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KL.Data.Trie
+{
+
+    public interface IFoldingTrie<TLabel, TData> : ITrie<TLabel, TData>
+    {
+        TResult Fold<TAccumulate, TResult>(TLabel[] query, TAccumulate seed, Func<TAccumulate, TData, TAccumulate> folder, Func<TAccumulate, TResult> resultSelector);
+    }
+
+    public class FoldingTrie<TLabel, TData> : Trie<TLabel, TData>, IFoldingTrie<TLabel, TData>
+    {
+        public TResult Fold<TAccumulate, TResult>(TLabel[] query, 
+            TAccumulate seed, 
+            Func<TAccumulate, TData, TAccumulate> folder, 
+            Func<TAccumulate, TResult> resultSelector)
+        {
+            int position = 0;
+            var accumulate = seed;
+            var child = (TrieNodeBase<TLabel, TData>)this;
+            while (!EndOfLabels(position, query))
+            {
+                child = child.GetChildOrNull(query, position);
+                if (child == null) break;
+
+                TrieNode<TLabel, TData> nextNode = (TrieNode<TLabel, TData>)child;
+                accumulate = folder(accumulate, nextNode.Data.FirstOrDefault());
+                position++;
+            }
+            return resultSelector(accumulate);
+        }
+    }
+
+}
